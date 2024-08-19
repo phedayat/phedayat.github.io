209ced453901de69f57adc3fc089231b8c514667
phedayat <phedayat@ucsd.edu>
2024-08-19 15:40:10 -0500

    Updated `document_transform.py` to write front matter

--- utils/typst_transform/document_transform.py
+++ utils/typst_transform/document_transform.py
@@ -1,5 +1,7 @@
 from typing import List
+from datetime import datetime
 from argparse import ArgumentParser
+from os.path import basename, extsep
 
 from components import (
     Details, 
@@ -15,6 +17,14 @@ from components import (
 parser = ArgumentParser()
 parser.add_argument("-i", "--input_file")
 args = parser.parse_args()
+
+metadata = {
+    "title": basename(args.input_file).split(extsep)[0],
+    "date": datetime.now().date().isoformat(),
+    "math": True,
+}
+metadata_s = "\n".join([f"{key}: {value}" for (key, value) in metadata.items()])
+front_matter = f"---\n{metadata_s}\n---\n\n"
 
 with open(args.input_file, "r+") as f:
     source = f.read()
@@ -33,4 +43,5 @@ with open(args.input_file, "r+") as f:
 
     f.seek(0)
     f.flush()
+    f.write(front_matter)
     f.write(source)
